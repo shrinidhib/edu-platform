fd1fb5e4c9bac1a8ae4700acd2a987726c775906
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-04-12 19:55:39 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,9 @@ const app=express()
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(cors())
+app.use(cors({
+    origin: ['https://edu-frontend-sage.vercel.app/']
+}))
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
