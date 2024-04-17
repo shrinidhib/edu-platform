3cf0e10bcf6593403a97829041bb786bba34951b
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-04-17 17:57:42 +0530

    with sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,6 +21,7 @@ import DisplayForum from './pages/DisplayForum.js'
 import AllDoc from './pages/AllDoc.js'
 
 import { UserDetails } from './pages/UserDetails.js'
+import SideBar from './components/SideBar.js'
 
 const App = () => {
   const {user}=useAuthContext()
@@ -37,7 +38,7 @@ const App = () => {
       <BrowserRouter>
       <Navbar/>
         <div className='layout'>
-           
+           {user && <SideBar/>}
             
           <div className='pages'>
             <Routes>
